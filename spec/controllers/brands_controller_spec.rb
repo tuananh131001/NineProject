cec46a83f39d3193f47a56998da6daf0f487e63a
william <s3864077@rmit.edu.vn>
2023-09-04 17:10:21 +0700

    Update some spec brand

--- spec/controllers/brands_controller_spec.rb
+++ spec/controllers/brands_controller_spec.rb
@@ -11,51 +11,47 @@ RSpec.describe BrandsController do
   end
 
   describe 'GET #index' do
-    it 'returns a successful response' do
+    subject { get :index }
 
-      get :index
-
-      expect(response).to have_http_status(:ok)
-    end
+    it { is_expected.to have_http_status(:ok) }
   end
 
   describe 'GET #show' do
+    subject { get :show, params: { id: brand.id } }
+
     let(:brand) { create(:brand) }
 
-    context 'when the request is valid' do
-      it 'returns a successful response' do
-
-        get :show, params: { id: brand.id }
-        expect(response).to have_http_status(:ok)
-      end
-    end
+    it { is_expected.to have_http_status(:ok) }
   end
 
   describe 'POST #create' do
-    let(:valid_attributes) { { name: 'Brand Name', rate: 5, headquarters: 'HQ', industry: 'Industry', founder: 'Founder', website: 'www.example.com', status: 'active' } }
+    subject { post :create, params: valid_attributes }
 
     context 'when the request is valid' do
+      let(:valid_attributes) { { name: 'Brand Name', rate: 5, headquarters: 'HQ', industry: 'Industry', founder: 'Founder', website: 'www.example.com', status: 'active' } }
+
       it 'creates a new brand' do
-        post :create, params: valid_attributes
-        expect(response).to have_http_status(:created)
+        expect(subject).to have_http_status(:created)
         expect(Brand.count).to eq(1)
       end
+    end
 
-      it 'returns errors for invalid brand data' do
-        post :create, params: { brand: { name: '' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
+    context 'when the request is invalid' do
+      let(:valid_attributes) { { brand: { name: '' } } }
+
+      it { is_expected.to have_http_status(:unprocessable_entity) }
     end
   end
 
   describe 'PATCH #status' do
+    subject { patch :status, params: { id: brand.id } }
+
     let(:brand) { create(:brand) }
 
     context 'when the request is valid' do
 
       it 'toggles the brand status' do
-        patch :status, params: { id: brand.id }
-        expect(response).to have_http_status(:created)
+        expect(subject).to have_http_status(:created)
         brand.reload
         expect(brand.status).to eq('inactive')
       end
@@ -64,13 +60,13 @@ RSpec.describe BrandsController do
   end
 
   describe 'DELETE #destroy' do
+    subject { delete :destroy, params: { id: brand.id } }
+
     let!(:brand) { create(:brand) }
 
     context 'when the request is valid' do
-
       it 'deletes the brand' do
-        delete :destroy, params: { id: brand.id }
-        expect(response).to have_http_status(:ok)
+        expect(subject).to have_http_status(:ok)
         expect(Brand.count).to eq(0)
       end
     end
